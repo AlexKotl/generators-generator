29aea63da636ad89d03d9235ff3a1545cef9fe42
Alex Kotliarov <slicer256@gmail.com>
2022-08-08 11:25:56 +0200

    basic api setup

---
+++ api/app/Http/Controllers/API/GeneratorController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class GeneratorController extends Controller
+{
+    public function list()
+    {
+        return response()->json([]);
+    }
+}
--- api/database/migrations/2022_08_08_085311_create_generators.php
+++ api/database/migrations/2022_08_08_085311_create_generators.php
@@ -15,6 +15,31 @@ return new class extends Migration
     {
         Schema::create('generators', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->index()->nullable();
+            $table->string('title');
+            $table->text('description');
+            $table->integer('rating')->nullable()->default(0);
+            $table->integer('flag')->default(1);
+            $table->timestamps();
+        });
+
+        Schema::create('generator_steps', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('generator_id')->index();
+            $table->integer('order')->nullable()->default(null);
+        });
+
+        Schema::create('generator_step_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('generator_step_id')->index();
+            $table->string('title');
+            $table->integer('order')->nullable()->default(null);
+        });
+
+        Schema::create('saved', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->index();
+            $table->string('title');
             $table->timestamps();
         });
     }
@@ -27,5 +52,7 @@ return new class extends Migration
     public function down()
     {
         Schema::dropIfExists('generators');
+        Schema::dropIfExists('generator_steps');
+        Schema::dropIfExists('generator_step_items');
     }
 };

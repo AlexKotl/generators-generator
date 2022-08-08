694b1b6f81077527e50b7980e36f4719b767bc74
Alex Kotliarov <slicer256@gmail.com>
2022-08-08 14:37:32 +0200

    basic navigation

--- app.vue
+++ app.vue
@@ -1,3 +1,29 @@
 <template>
-  <div class="text-3xl font-bold underline">Hello mom</div>
+  <div>
+    <body class="bg-slate-600 text-white"></body>
+
+    <div class="cotainer px-5">
+      <div class="flex justify-between items-center">
+        <img src="~/assets/images/logo.svg" class="mw-100" width="200" alt="" />
+
+        <div class="leading-7">
+          <NuxtLink
+            v-for="(link, i) of [
+              { title: 'Home', url: '/' },
+              { title: 'Create', url: '/create' },
+              { title: 'About', url: '/about' },
+            ]"
+            :to="link.url"
+            class="px-3 py-5 rounded font-bold text-slate-300 hover:bg-gray-100 hover:text-slate-800"
+            :key="i"
+            >{{ link.title }}</NuxtLink
+          >
+        </div>
+      </div>
+
+      <div>
+        <NuxtPage />
+      </div>
+    </div>
+  </div>
 </template>
